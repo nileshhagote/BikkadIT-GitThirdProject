4d73248a71b11353dee35f98f2b840a15141b1e0
nileshhagote <nilesh.hagote8275@gmail.com>
2022-08-04 22:32:56 +0530

    Student mgt sys-ServiceI Interface Created

---
+++ src/main/java/com/example/serviceI/StudentServiceI.java
@@ -0,0 +1,19 @@
+package com.example.serviceI;
+
+import java.util.List;
+
+import com.example.model.Student;
+
+
+
+public interface StudentServiceI {
+	
+	public int addStudent(Student student) ;
+	
+	public List<Student> getAllStudent();
+	
+	public Student getStudentById();
+	
+	
+
+}
