38f119470ff3c56354e17da9bcd0299bbf90770e
nileshhagote <nilesh.hagote8275@gmail.com>
2022-08-04 23:06:03 +0530

    Student mgt sys-StudentServiceImpl class added

---
+++ src/main/java/com/example/serviceI/StudentServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.serviceI;
+
+import java.util.List;
+
+import com.example.model.Student;
+
+public class StudentServiceImpl implements StudentServiceI{
+
+	@Override
+	public int addStudent(Student student) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Student> getAllStudent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getStudentById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
